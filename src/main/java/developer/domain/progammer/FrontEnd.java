da98f747219360bfe796bac095ddc25b6dd92c76
is2js <tingstyle1@gmail.com>
2022-06-19 16:40:30 +0900

    feat: apply template method pattern to Programmer

--- src/main/java/developer/domain/progammer/FrontEnd.java
+++ src/main/java/developer/domain/progammer/FrontEnd.java
@@ -6,23 +6,22 @@ import developer.domain.Program;
 import developer.domain.paper.Client;
 import developer.domain.paper.Paper;
 
-public class FrontEnd implements Programmer {
+public class FrontEnd  extends Programmer {
 
     private Language language;
     private Library library;
 
     @Override
-    public Program makeProgram(final Paper paper) {
+    protected void setData(final Paper paper) {
         if (paper instanceof Client) {
             final Client pb = (Client) paper;
             language = pb.getLanguage();
             library = pb.getLibrary();
         }
-
-        return makeFrontEndProgram("프론트 프로그램");
     }
 
-    private Program makeFrontEndProgram(final String name) {
-        return new Program(name);
+    @Override
+    protected Program makeProgram() {
+        return new Program("프론트 프로그램");
     }
 }
